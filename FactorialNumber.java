34ce953c34012722f1ec7fbd1d12ba2083159090
SoniShreya123 <117342475+SoniShreya123@users.noreply.github.com>
2022-11-05 12:09:45 +0530

    Add files via upload

---
+++ FactorialNumber.java
@@ -0,0 +1,24 @@
+package basicprogram;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc =new Scanner(System.in);
+		  int  i,num,fact=1 ; //declare variables
+		  
+		  
+		  System.out.println(" Enter a number"); //taking input from user
+		  		num= sc.nextInt();
+		  		for(i=1;i<=num;i++)
+		  		{
+		  			  fact=fact*i;
+		  		}
+		  		
+		          System.out.println(" The factorial of "  +num+ "!= " +fact);
+		    
+	}
+
+}
