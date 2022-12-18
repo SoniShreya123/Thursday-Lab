d1acc3f0307f204361ac539dd2a609c77008e9d4
SoniShreya123 <117342475+SoniShreya123@users.noreply.github.com>
2022-12-18 22:01:35 +0530

    Add files via upload

---
+++ 15-12-22/JoinTwoString.java
@@ -0,0 +1,20 @@
+package arrayprogram;
+
+import java.util.Scanner;
+
+public class JoinTwoString {
+	public static void main(String[] args)
+	{
+		String str1,str2;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the 1st string");
+		str1=sc.nextLine();
+		
+		System.out.println("Enter the 2nd string");
+		str2=sc.nextLine();
+		
+		System.out.println(str1.concat(str2));
+	}
+
+}
